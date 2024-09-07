e92424b0257694f6e436edd19748af4ea496da44
Naman Thanki <stuniz7855@gmail.com>
2024-09-07 15:57:23 +0530

    Wrapped in Toast Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,21 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Toaster
+					position="top-right"
+					toastOptions={{
+						className: "bg-gray-800 text-white",
+						success: {
+							className: "bg-green-500",
+						},
+						error: {
+							className: "bg-red-500",
+						},
+					}}
+				/>
+				{children}
+			</body>
 		</html>
 	);
 }
