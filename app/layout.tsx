e5d0f8eae089e9b3fbcb37ad65eccd494b59ff54
Naman Thanki <stuniz7855@gmail.com>
2024-09-08 14:50:18 +0530

    Render navbar on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
 						},
 					}}
 				/>
+				<Navbar />
 				{children}
 			</body>
 		</html>
