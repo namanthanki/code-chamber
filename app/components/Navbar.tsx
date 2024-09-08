86e7d88d90946a48221a78ad1c4414bc259cd18b
Naman Thanki <stuniz7855@gmail.com>
2024-09-08 14:51:24 +0530

    Implemented navbar that conditionally renders the contents based on the loggedIn state

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,9 +3,53 @@
 import Link from "next/link";
 import { useAuthModalStore } from "../stores/authModalStore";
 import { FaCode } from "react-icons/fa";
+import { useAuthStore } from "../stores/authStore";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import axios from "axios";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function Navbar() {
 	const { open } = useAuthModalStore();
+	const router = useRouter();
+	const pathname = usePathname();
+	const {
+		isLoading,
+		isLoggedIn,
+		user,
+		setIsLoading,
+		setIsLoggedIn,
+		setUser,
+	} = useAuthStore();
+
+	useEffect(() => {
+		const checkLoggedIn = async () => {
+			setIsLoading(true);
+			try {
+				const response = await axios.get("/api/users/me");
+				setIsLoggedIn(true);
+				setUser(response.data.user);
+			} catch (error) {
+				console.log(error);
+			}
+			setIsLoading(false);
+		};
+
+		checkLoggedIn();
+	}, [pathname]);
+
+	const handleLogout = async () => {
+		setIsLoading(true);
+		try {
+			await axios.get("/api/users/logout");
+			setIsLoggedIn(false);
+			setUser(null);
+			router.push("/");
+		} catch (error) {
+			console.log(error);
+		}
+		setIsLoading(false);
+	};
 
 	return (
 		<nav className="bg-gray-900 shadow-md">
@@ -18,14 +62,28 @@ export default function Navbar() {
 						<FaCode className="h-8 w-8 text-indigo-500 mr-2" />
 						Code Chamber
 					</Link>
-					<div className="flex items-center">
+					{isLoading ? (
+						<LoadingSpinner />
+					) : isLoggedIn ? (
+						<div className="flex items-center">
+							<p className="text-white mr-4">
+								Welcome, {user?.name}
+							</p>
+							<button
+								onClick={handleLogout}
+								className="ml-4 bg-red-500 text-white px-4 py-2 rounded-md"
+							>
+								Logout
+							</button>
+						</div>
+					) : (
 						<button
-							className="bg-indigo-600 hover:bg-indigo-700 text-white px-4 py-2 rounded-md text-sm font-medium transition duration-300 ease-in-out"
 							onClick={open}
+							className="bg-indigo-500 text-white px-4 py-2 rounded-md"
 						>
-							Sign In
+							Login
 						</button>
-					</div>
+					)}
 				</div>
 			</div>
 		</nav>
