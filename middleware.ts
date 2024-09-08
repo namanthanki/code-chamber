9cd5afcc0fd13cc74be30afc4efb4c592c0b97a2
Naman Thanki <stuniz7855@gmail.com>
2024-09-08 13:55:58 +0530

    middleware bug fixed

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+	const path = request.nextUrl.pathname;
+	const isPublicPath = path === "/" || path.includes("/auth");
+	const token = request.cookies.get("token")?.value;
+	if (isPublicPath && token) {
+		return NextResponse.redirect(new URL("/profile", request.url));
+	}
+	if (!isPublicPath && !token) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+}
+
+export const config = {
+	matcher: ["/", "/auth/verify-email", "/profile"],
+};
