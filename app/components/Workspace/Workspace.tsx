c607663e415146705cb93cc51c14f8f6c4ea865c
Naman Thanki <stuniz7855@gmail.com>
2024-11-10 03:35:10 +0530

    state needed for problem solving

--- app/components/Workspace/Workspace.tsx
+++ app/components/Workspace/Workspace.tsx
@@ -4,16 +4,35 @@ import Split from "react-split";
 import ProblemDescription from "../ProblemDescription/ProblemDescription";
 import Playground from "../Playground/Playground";
 import { Problem } from "@/app/utils/types/problem";
+import { useState } from "react";
+import Confetti from "react-confetti";
+import useWindowSize from "@/app/hooks/useWindowSize";
 
 type WorkspaceProps = {
 	problem: Problem;
 };
 
 export default function Workspace({ problem }: WorkspaceProps) {
+	const { width, height } = useWindowSize();
+	const [success, setSuccess] = useState(false);
+	const [solved, setSolved] = useState(false);
+
 	return (
 		<Split className="split">
-			<ProblemDescription problem={problem} />
-			<Playground problem={problem} />
+			<ProblemDescription problem={problem} _solved={solved} />
+			<Playground
+				problem={problem}
+				setSuccess={setSuccess}
+				setSolved={setSolved}
+			/>
+			{success && (
+				<Confetti
+					gravity={0.3}
+					tweenDuration={4000}
+					width={width - 1}
+					height={height - 1}
+				/>
+			)}
 		</Split>
 	);
 }
