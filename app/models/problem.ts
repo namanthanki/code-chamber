31becc7d164e59029219c11ea61be60a9845660e
Naman Thanki <stuniz7855@gmail.com>
2024-11-23 03:58:19 +0530

    modified problem model

--- app/models/problem.ts
+++ app/models/problem.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
+import { Example } from "../utils/types/problem";
 
 export interface Problem {
 	id: string;
 	title: string;
+	problemStatement: string;
+	examples: Example[];
+	constraints: string;
+	starterCode: string;
+	handlerFunction: string;
 	difficulty: string;
 	category: string;
 	videoId?: string;
@@ -12,11 +18,28 @@ export interface Problem {
 	dislikes: number;
 }
 
+const ExampleSchema = new mongoose.Schema({
+	id: { type: Number, required: true },
+	inputText: { type: String, required: true },
+	outputText: { type: String, required: true },
+	explanation: { type: String },
+	img: { type: String },
+});
+
 export const ProblemSchema = new mongoose.Schema<Problem>(
 	{
 		id: { type: String, required: true },
 		title: { type: String, required: true },
-		difficulty: { type: String, required: true },
+		problemStatement: { type: String, required: true },
+		examples: [ExampleSchema],
+		constraints: { type: String, required: true },
+		starterCode: { type: String, required: true },
+		handlerFunction: { type: String, required: true },
+		difficulty: {
+			type: String,
+			required: true,
+			enum: ["Easy", "Medium", "Hard"],
+		},
 		category: { type: String, required: true },
 		videoId: { type: String },
 		link: { type: String },
@@ -26,6 +49,10 @@ export const ProblemSchema = new mongoose.Schema<Problem>(
 	},
 	{ timestamps: true }
 );
+
+ProblemSchema.index({ id: 1 }, { unique: true });
+ProblemSchema.index({ difficulty: 1 });
+ProblemSchema.index({ category: 1 });
 
 const ProblemModel =
 	mongoose.models.problems ||
